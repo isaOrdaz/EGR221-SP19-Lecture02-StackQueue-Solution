e9b73c07114fb469e30e7a1439f7fd8acf77578c
Mi Kyung Han <mhan@calbaptist.edu>
2019-01-11 22:37:06 -0800

    Adding solution for interleave

---
+++ src/solution/InterleaveFinished.java
@@ -0,0 +1,46 @@
+package solution;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+/**
+ * Created by mhan on 1/18/2017.
+ */
+public class InterleaveFinished {
+    public static void main (String[] args){
+        Queue<Integer> q = new LinkedList<>();
+        int[] array = {1,2,3,4,100,200,300,400};
+        for(int i : array){
+            q.add(i);
+        }
+        interleave(q);
+        System.out.println(q); //desired result is [1, 100, 2, 200, 3, 300, 4, 400]
+    }
+
+    public static void interleave(Queue<Integer> q) {
+        Stack<Integer> s = new Stack<>();
+        if(q.size()%2 != 0)
+            throw new IllegalArgumentException();
+
+        int size = q.size();
+        for(int i = 0 ; i < size/ 2; i++){
+            s.push(q.remove());
+        }
+        while(!s.isEmpty())
+            q.add(s.pop());
+
+        for(int i = 0 ; i < size; i++){
+            if( i < size /2 ) {
+                q.add(q.remove());
+            } else {
+                s.push(q.remove());
+            }
+        }
+
+        while(!s.isEmpty()) {
+            q.add(s.pop());
+            q.add(q.remove());
+        }
+    }
+}
